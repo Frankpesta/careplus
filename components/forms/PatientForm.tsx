32a57e4f0bb8760ed5a1465b8cc26f76d8275090
Frankpesta <franklinolisaemeka3@gmail.com>
2024-09-28 05:38:35 +0100

    feat:setting up and creating the login page

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation";
+
+export enum FormFieldType {
+	INPUT = "input",
+	TEXTAREA = "textarea",
+	PHONE_INPUT = "phoneInput",
+	CHECKBOX = "checkbox",
+	DATE_PICKER = "datePicker",
+	SELECT = "select",
+	SKELETON = "skeleton",
+}
+
+const PatientForm = () => {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const form = useForm<z.infer<typeof UserFormValidation>>({
+		resolver: zodResolver(UserFormValidation),
+		defaultValues: {
+			name: "",
+			email: "",
+			phone: "",
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof UserFormValidation>) {
+		console.log(values);
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+				<section className="mb-12 space-y-4">
+					<h1 className="header">Hi there 👋</h1>
+					<p className="text-dark-700">Schedule your first appointment.</p>
+				</section>
+				<CustomFormField
+					control={form.control}
+					fieldType={FormFieldType.INPUT}
+					name="name"
+					label="Full name"
+					placeholder="John Doe"
+					iconSrc="/assets/icons/user.svg"
+					iconAlt="user"
+				/>
+				<CustomFormField
+					control={form.control}
+					fieldType={FormFieldType.INPUT}
+					name="email"
+					label="Email"
+					placeholder="johndoe@gmail.com"
+					iconSrc="/assets/icons/email.svg"
+					iconAlt="email"
+				/>
+
+				<CustomFormField
+					control={form.control}
+					fieldType={FormFieldType.PHONE_INPUT}
+					name="phone"
+					label="Phone number"
+					placeholder="(555) 123-5876"
+				/>
+
+				<SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+			</form>
+		</Form>
+	);
+};
+
+export default PatientForm;
